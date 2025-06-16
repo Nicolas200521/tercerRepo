751aeb76de08b2ac58fe66a4602149fd1d9f8676
Nicolas Guevara <nicolase.guevarap@ecci.edu.co>
2025-06-15 22:03:14 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                # Nombre del paquete
+    version="0.1.0",                                  # Versión inicial
+    packages=find_packages(),                         # Paquetes a incluir
+    description="Un paquete pip simple de saludo",    # Breve descripción
+    author="Nicolas Guevara",                         # Tu nombre
+    author_email="nicolase.guevarap@ecci.edu.co",     # Tu correo electrónico
+    url="https://github.com/Nicolas200521/tercerRepo",       # URL del proyecto
+)
